992c8b67d988184706487a503c4a783faf2f654e
Jun <sunjun.ee@tngdigital.com.my>
2021-07-08 15:38:13 +0800

    yay

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -50,32 +50,7 @@ public class Main {
   public static void main(String[] args) throws Exception {
     SpringApplication.run(Main.class, args);
   }
-
-  @RequestMapping("/test")
-  String index() {
-    return "testing";
-  }
-
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+  
 
   @Bean
   public DataSource dataSource() throws SQLException {
--- src/main/java/com/example/service/CreateUserService.java
+++ src/main/java/com/example/service/CreateUserService.java
@@ -6,6 +6,7 @@ import com.example.result.CreateUserResult;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.sql.DataSource;
@@ -21,14 +22,21 @@ public class CreateUserService {
         CreateUserResult createUserResult = new CreateUserResult();
         createUserResult.setId(mobile_no);
         try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement st = connection.prepareStatement("INSERT INTO customer (name,mobile_no) VALUES (?, ?)");
+            PreparedStatement st = connection.prepareStatement("SELECT * FROM customer WHERE mobile_no = ?");
+            st.setString(1,mobile_no);
+            ResultSet resultSet = st.executeQuery();
+            if (!resultSet.next()){
+                createUserResult.setSuccess(true);
+                return createUserResult;
+            }
+
+            st = connection.prepareStatement("INSERT INTO customer (name,mobile_no) VALUES (?, ?)");
             st.setString(1,name);
             st.setString(2,mobile_no);
             st.executeUpdate();
             createUserResult.setSuccess(true);
         } catch (Exception e){
             createUserResult.setSuccess(false);
-            createUserResult.setId("fail jor");
             e.printStackTrace();
         }
         return createUserResult;
